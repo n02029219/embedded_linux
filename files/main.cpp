fd3936092bfe55287321aae68967213b805d3655
Mike Facelle <n02029219@hawkmail.newpaltz.edu>
2013-11-28 22:36:13 -0500

    Updated main.cpp, project.h, and added ppt3.pdf

--- files/main.cpp
+++ files/main.cpp
@@ -29,7 +29,7 @@ int main (int argc, const char * argv[])
     // use Canny for edge-detection
     // source, destinaton, threshold1, threshold2, aperturesize=3, L2gradient=false
     blur(dst, dst, Size(3,3));
-    Canny(src, dst, 100, 100, 3);
+    Canny(src, dst, CANNY_T1, CANNY_T2, CANNY_APERTURE);
     cvtColor(dst, cdst, CV_GRAY2RGB);
     
     // time of canny
@@ -46,18 +46,38 @@ int main (int argc, const char * argv[])
     // maxLineGap: The maximum gap between two points to be considered in the same line.
     
     vector<Vec4i> lines;
-    HoughLinesP(dst, lines, 1, CV_PI/180, 25, 50, 20 );
+    HoughLinesP(dst, lines, 1, CV_PI/180, HLINES_THRESH, HLINES_MINLINE, HLINES_MINGAP);
     
     // filter out horizontal lines
     remove_horizontal(&lines);
     
+    //vector<Vec4i> lane_lines = combine_lines(lines);
+    //lane_lines = extend_lines(lane_lines, dst.cols, dst.rows);
+    
+    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+    cout << "size of lines: " << lines.size() << endl;
+    //cout << "size of lane_lines: " << lane_lines.size() << endl;
+    cout << "width: " << dst.cols << "  height: " << dst.rows << endl;
+    //line(cdst, Point(0,0), Point(100,100), Scalar(255,255,255), 2, CV_AA);
+    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+
     // display result:
     for( size_t i = 0; i < lines.size(); i++ )
     {
         Vec4i l = lines[i];
-        line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,255,255), 1, CV_AA);
-        cout << "(" << l[0] << ", " << l[1] << ") \t(" << l[2] << ", " << l[3] << ")" << endl;
+        line( cdst, Point(l[X1], l[Y1]), Point(l[X2], l[Y2]), Scalar(0,255,255), 1, CV_AA);
+        cout << i << " (" << l[X1] << "," << l[Y1] << ") \t(" << l[X2] << "," << l[Y2] << ")" << endl; 
     }
+    cout << "------" << endl;
+    // display "lane lines"
+    /*
+    for( size_t i = 0; i < lane_lines.size(); i++ )
+    {
+        Vec4i l = lane_lines[i];
+        //line( cdst, Point(l[X1], l[Y1]), Point(l[X2], l[Y2]), Scalar(0,255,255), 2, CV_AA);
+        cout << i << " (" << l[X1] << "," << l[Y1] << ") \t(" << l[X2] << "," << l[Y2] << ")" << endl; 
+    }*/
+    
     
     // total end time
     clock_t end = clock();
@@ -75,7 +95,7 @@ int main (int argc, const char * argv[])
     // display output
     namedWindow("original image");
     imshow("original image", src);
-    waitKey(1000);  // waits 3 seconds
+    waitKey(1000);  // waits 1 seconds
     namedWindow("lines detected");
     imshow("lines detected", cdst);
     waitKey();
@@ -97,12 +117,24 @@ void remove_horizontal(vector<Vec4i> * lines)
             lines->erase(lines->begin() + i--);
 }
 
-// ------------------------
+// removes two lines from a vector
+void remove_lines(int i, int j, vector<Vec4i> *lines)
+{
+    // remove greater index first so it doesn't shift lower index
+    if (i > j) {
+        lines->erase(lines->begin() + i);
+        lines->erase(lines->begin() + j);
+    }
+    else {
+        lines->erase(lines->begin() + j);
+        lines->erase(lines->begin() + i);    
+    }
+}
 
 // determines if the slope of a line is greater than the minimum "horizontal" value
 bool horizontal(Vec4i l)
 {
-    if (slope(l) > HORIZONTAL_TOLERANCE)    // if slope greater than allowed value
+    if (abs(slope(l)) > HORIZONTAL_TOLERANCE)    // if slope greater than allowed value
         return false;
     else
         return true;
@@ -116,49 +148,169 @@ bool horizontal(Vec4i l)
 //  returns a new line vector
 vector<Vec4i> combine_lines(vector<Vec4i> lines)
 {
-    vector<Vec4i> new_lines;
+    vector<Vec4i> new_lines = lines;
     Vec4i l1, l2;
-    for (size_t i = 0; i < lines.size(); i++) {
-        l1 = lines[i];
-        for (size_t j = 0; j < lines.size(); j++) {
-            l2 = lines[j];
-            if (!greater_than(l1,l2))    // if l1 < l2
-                swap(l1,l2);
-            if (adjacent(l1,l2)) {
-                // fix by making a new line:
-                //  x[1,2] = avg(l1_x,l2_x), y[1,2] = avg(l1_y,l2_y)                
-                int x1 = mean(l1[0], l2[0]);
-                int y1 = mean(l1[1], l2[1]);
-                int x2 = mean(l1[2], l2[2]);
-                int y2 = mean(l1[3], l2[3]);
-                new_lines.push_back(Vec4i (x1,y1,x2,y2));
+    bool same = false;
+    
+    for (size_t i = 0; i < new_lines.size(); ) {
+        cout << "size: " << new_lines.size() << endl;
+        l1 = new_lines[i];
+        
+        for (size_t j = 0; j < new_lines.size(); j++) {
+            // skip i==j, unless at the last index
+            if (i == j) {
+                if (j == new_lines.size()-1)
+                    continue;
+                j++;
             }
-            else if (seperated(l1,l2)) {
-                // fix by making a new line:
-                //  min(x,y) : min(l1_lo, l2_lo)
-                //  max(x,y) : max(l1_hi, l2_hi)
-                int x1 = (int)min(l1[0], l2[0]);
-                int y1 = (int)min(l1[1], l2[1]);
-                int x2 = (int)max(l1[2], l2[2]);
-                int y2 = (int)max(l1[3], l2[3]);
-                new_lines.push_back(Vec4i (x1,y1,x2,y2));
+            same = false;
+            l2 = new_lines[j];
+            
+            // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+            cout << i << "," << j << "\t" << endl;
+            cout << "\tl1(" << l1[X1] << "," << l1[Y1] << ") (" << l1[X2] << "," << l1[Y2] << ")   ";
+            cout << slope(l1) << ",   " << x_intercept(l1) << endl;
+            cout << "\tl2(" << l2[X1] << "," << l2[Y1] << ") (" << l2[X2] << "," << l2[Y2] << ")   ";
+            cout << slope(l2) << ",   " << x_intercept(l2) << endl;
+            // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+            
+            if(same_line(l1,l2)) {
+                same = true;
+                // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                cout << "\tsame line" << endl;
+                // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                // swap lines so l1 is higher up than l2
+                if (greater_than(l1, l2))
+                    swap(&l1, &l2);
+                
+                // if line found, remove both previous lines and add created line
+                if (adjacent(l1,l2)) {
+                    cout << "\tlines adjacent" << endl;
+                    // fix by making a new line:
+                    //  x[1,2] = avg(l1_x,l2_x), y[1,2] = avg(l1_y,l2_y)   
+                    // first find min and max y-values (in case slopes aren't same sign)
+                    // reverse min/max since 0,0 in TOP LEFT
+                    double s1 = slope(l1);
+                    double s2 = slope(l2);
+                    int min_y1 = l1[Y1], max_y1 = l1[Y2], min_y2 = l2[Y1], max_y2 = l2[Y2];
+                    if ((s1 >= 0 && s2 < 0) || (s1 < 0 && s2 >= 0)) {
+                        min_y1 = slope(l1) >= 0 ?   l1[Y1] : l1[Y2];
+                        max_y1 = slope(l1) >= 0 ?   l1[Y2] : l1[Y1];
+                        min_y2 = slope(l2) >= 0 ?   l2[Y1] : l2[Y2];
+                        max_y2 = slope(l2) >= 0 ?   l2[Y2] : l2[Y1];
+                    }
+                    int x1 = (int)mean(l1[X1], l2[X1]);
+                    int x2 = (int)mean(l1[X2], l2[X2]);
+                    int y1 = (int)mean(min_y1, min_y2);
+                    int y2 = (int)mean(max_y1, max_y2);        
+                    // remove lines if concatenated
+                    remove_lines((int)i, (int)j, &new_lines);
+                    // add to new and old line vectors
+                    Vec4i line (x1,y1,x2,y2);
+                    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                    cout << "\tFIX(" << line[X1] << "," << line[Y1] << ") (" << line[X2] << "," << line[Y2] << ")   " << endl;
+                    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                    new_lines.push_back(line);
+                    break;  // because lines[i] no longer exists
+                }
+                else if (seperated(l1,l2)) {
+                    cout << "\tlines separated" << endl;
+                    // fix by making a new line:
+                    //  min(x,y) : min(l1_lo, l2_lo)
+                    //  max(x,y) : max(l1_hi, l2_hi)
+                    // first find min and max y-values (in case slopes aren't same sign)
+                    // reverse min/max since 0,0 in TOP LEFT
+                    double s1 = slope(l1);
+                    double s2 = slope(l2);
+                    int min_y1 = l1[Y1], max_y1 = l1[Y2], min_y2 = l2[Y1], max_y2 = l2[Y2];
+                    if ((s1 >= 0 && s2 < 0) || (s1 < 0 && s2 >= 0)) {
+                        min_y1 = slope(l1) >= 0 ?   l1[Y1] : l1[Y2];
+                        max_y1 = slope(l1) >= 0 ?   l1[Y2] : l1[Y1];
+                        min_y2 = slope(l2) >= 0 ?   l2[Y1] : l2[Y2];
+                        max_y2 = slope(l2) >= 0 ?   l2[Y2] : l2[Y1];
+                    }
+                    int x1 = min(l1[X1], l2[X1]);
+                    int x2 = max(l1[X2], l2[X2]);
+                    int y1 = max(min_y1, min_y2);
+                    int y2 = min(max_y1, max_y2); 
+                    // remove lines if concatenated
+                    remove_lines((int)i, (int)j, &new_lines);
+                    // add to new and old lines vectors
+                    Vec4i line (x1,y1,x2,y2);
+                    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                    cout << "\tFIX(" << line[X1] << "," << line[Y1] << ") (" << line[X2] << "," << line[Y2] << ")   " << endl;
+                    // -=-=-=-=-=-=-=-=-=-=-=-=- DEBUGGING -=-=-=-=-=-=-=-=-=-=-=-=-
+                    new_lines.push_back(line);
+                    break;  // because lines[i] no longer exists
+                }
             }
-            
-            
-            l1 = lines[i];  // restore, in case value was swapped
+            // restore, in case value was swapped (for future i,j pairs)
+            // only happens if lines were the same but not adjacent or separated (shouldn't happen)
+            l1 = new_lines[i];
         }
+        if (!same)
+            i++;
     }
+    return new_lines;
+}
+
+
+// extends lines so that they reach the bottom/edge of the screen
+//  if smallest x (will always be X1) is near 0, make it 0
+//  if largest x (will always be X2) is near the width, make it width (at correct y)
+// must determine largest y first
+//  if largest y is near height, make it height (at correct x)
+//  don't care about if y is near 0 because that would be the sky in the image
+// NEAR_EDGE value assumes lines will probably be close to one edge, so = 150px
+vector<Vec4i> extend_lines(vector<Vec4i> lines, int width, int height)
+{
+    vector<Vec4i> new_lines;
+    new_lines.clear();  // just to be sure it's empty
+    for (size_t i = 0; i < lines.size(); i++) {
+        Vec4i l = lines[i];
+        // if any points are near an edge, update using y = mx + b
+        // x1 always less than x2 (way HoughLines stores them)
+        if (l[X1] < NEAR_EDGE) {
+            l[X1] = 0;
+            l[Y1] = y_intercept(l);
+        }
+        else if (l[X2] > width-NEAR_EDGE) {
+            l[X2] = width;
+            l[Y2] = slope(l)*l[X2] + y_intercept(l);
+        }
+        // x = (y-b)/m
+        else if ((l[Y1] > height - NEAR_EDGE) || (l[Y2] > height - NEAR_EDGE)) {
+            if (l[Y1] > l[Y2])
+                if (l[Y1] > height - NEAR_EDGE) {
+                    l[Y1] = height;
+                    l[X1] = (l[Y1] - y_intercept(l)) / slope(l);
+                }
+                else    // need this else to exit the if block
+                    ;
+            else 
+                if (l[Y2] > height - NEAR_EDGE) {
+                    l[Y2] = height;
+                    l[X2] = (l[Y2] - y_intercept(l)) / slope(l);
+                }
+        }
+        
+        new_lines.push_back(Vec4i(l[X1],l[Y1],l[X2],l[Y2]));
+    }
+    
     return new_lines;
 }
 
 // ------------------------
 
-// determines if line l1 > l2
-//  uses hypotenuse to the farthest point to do so
-//  (this may not be the best way to go about doing this)
+// assumes l1,l2 are "same" lines
+// determines if l1 is higher up than l2, using y-coordinate of furthest point
+// uses highest y-coordinate for each line
 bool greater_than(Vec4i l1, Vec4i l2)
 {
-    return hypotenuse(l1[2],l1[3]) > hypotenuse(l2[2],l2[3]) ?    true : false;
+    // use min, not max because 0,0 is TOP LEFT
+    return min(l1[Y1],l1[Y2]) > min(l2[Y1],l2[Y2]) ? true : false;
 }
 
 // swaps two lines (to pass to functions in the form: greater,lesser)
@@ -171,60 +323,68 @@ void swap(Vec4i *l1, Vec4i *l2)
 
 // ------------------------
 
-// assumes l1 > l2
+// if lines are the "same"
+// same slope, same x-intercept (within tolerance)
+bool same_line(Vec4i l1, Vec4i l2)
+{
+    // if x-ints aren't within tolerance, return false
+    double xint1 = abs(x_intercept(l1));
+    double xint2 = abs(x_intercept(l2));
+    if (!(xint2 > xint1-POINT_TOLERANCE && xint2 < xint1+POINT_TOLERANCE))
+        return false;
+    
+    // if slopes aren't within tolerance, return false
+    double s1 = slope(l1);
+    double s2 = slope(l2);
+    if (s1 >= 0 && s2 >= 0) {       // both positive
+        if (!(s2 > s1-(s1*SLOPE_TOLERANCE) && s2 < s1+(s1*SLOPE_TOLERANCE)))
+            return false;
+    }
+    else if (s1 < 0 && s2 < 0) {    // both negative
+        if (!(s2 > s1+(s1*SLOPE_TOLERANCE) && s2 < s1-(s1*SLOPE_TOLERANCE)))
+            return false;
+    }
+    else {  // slopes only equal if absolute values are close (because of different signs)
+        s1 = abs(s1);
+        s2 = abs(s2);
+        if (!(s2 > s1-(s1*SLOPE_TOLERANCE) && s2 < s1+(s1*SLOPE_TOLERANCE)))
+            return false;
+    }
+    
+    return true;
+}
+
+// assumes l1,l2 are "same" lines and l1 > l2
 // determines if two lines are adjacent (similar slope and position)
 //  if y-intercepts are "equal" (within tolerance)
 //  if slopes are "equal", and smallest l1(x,y) < greatest l2(x,y)
 // fix by making a new line: (in calling function)
 //  x[1,2] = avg(l1_x,l2_x), y[1,2] = avg(l1_y,l2_y)
+// reverse signs since 0,0 in TOP LEFT
 bool adjacent(Vec4i l1, Vec4i l2)
 {
-    // if slopes aren't within tolerance, return false
-    double s1 = slope(l1);
-    double s2 = slope(l2);
-    if (!(s2 > s1-(s1*SLOPE_TOLERANCE) && s2 < s1+(s1*SLOPE_TOLERANCE)))
-        return false;
-    
-    // if y-ints aren't within tolerance, return false
-    double yint1 = y_intercept(l1);
-    double yint2 = y_intercept(l2);
-    if (!(yint2 > yint1-POINT_TOLERANCE && yint2 < yint1+POINT_TOLERANCE))
-        return false;
-    
-    // use hypotenuse to determine difference in location between the two lines
-    if (hypotenuse(l1[0],l1[1] > hypotenuse(l2[2],l2[3])))
-        return false;   // seperated, not adjacent, if smallest(l1) > greatest(l2)
-    
-    // else:
+    // flip sign since 0,0 is TOP LEFT
+    int min1 = max(l1[Y1], l1[Y2]);
+    int max2 = min(l2[Y1], l2[Y2]);
+    if (min1 < max2)
+        return false;   // separated, not adjacent
+    // else
     return true;
 }
 
-// assumes l1 > l2
+// assumes l1,l2 are "same" lines and l1 > l2
 // determines if two lines are seperated (similar slope, position lines up)
 //  if y-intercepts are "equal" (within tolerance)
 //  if slopes are "equal" and smallest l1(x,y) > greatest l2(x,y)
-// fix by making a new line: (in calling function)
-//  min(x,y) : min(l1_lo, l2_lo)
-//  max(x,y) : max(l1_hi, l2_hi)
+// reverse signs since 0,0 in TOP LEFT
 bool seperated(Vec4i l1, Vec4i l2)
 {
-    // if slopes aren't within tolerance, return false
-    double s1 = slope(l1);
-    double s2 = slope(l2);
-    if (!(s2 > s1-(s1*SLOPE_TOLERANCE) && s2 < s1+(s1*SLOPE_TOLERANCE)))
-        return false;
-    
-    // if y-ints aren't within tolerance, return false
-    double yint1 = y_intercept(l1);
-    double yint2 = y_intercept(l2);
-    if (!(yint2 > yint1-POINT_TOLERANCE && yint2 < yint1+POINT_TOLERANCE))
-        return false;
-    
-    // use hypotenuse to determine difference in location between the two lines
-    if (hypotenuse(l1[0],l1[1] < hypotenuse(l2[2],l2[3])))
-        return false;   // adjacent, not seperated, if smallest(l1) < greatest(l2)
-    
-    // else:
+    // flip sign since 0,0 is TOP LEFT
+    int min1 = max(l1[Y1], l1[Y2]);
+    int max2 = min(l2[Y1], l2[Y2]);
+    if (min1 > max2)
+        return false;   // adjacent, not separated
+    // else
     return true;
 }
 
@@ -233,18 +393,19 @@ bool seperated(Vec4i l1, Vec4i l2)
 // returns the slope of the line passed
 double slope(Vec4i l)
 {
-    return abs((l[3]-l[1]) / (l[2]-l[0]));
+    return (double)(l[Y2]-l[Y1]) / (double)(l[X2]-l[X1]);
 }
 // determine the y-intercept of a line (using farthest point)
 // y0 = y - mx
 double y_intercept(Vec4i l)
 {
-    return l[3] - slope(l)*l[2];
+    return l[Y2] - slope(l)*l[X2];
 }
-// returns the hypotenuse
-double hypotenuse(double x, double y)
+// determine the y-intercept of a line (using farthest point)
+// x0 = x - y/m
+double x_intercept(Vec4i l)
 {
-    return sqrt(x*x + y*y);
+    return l[X2] - (double)l[Y2]/slope(l);
 }
 // gets the mean between two points (to be used with Vec4i points)
 int mean(int a, int b)
--- files/project.h
+++ files/project.h
@@ -15,26 +15,46 @@
 using namespace cv;
 using namespace std;
 
-const double HORIZONTAL_TOLERANCE = 0.3;    // how far from slope=0 is considered horizontal
-const double POINT_TOLERANCE = 10;          // how close (in px) for two lines to be "equal"
-const double SLOPE_TOLERANCE = 0.1;         // how close (in %) two lines slopes must be to be "equal"
+// constants for opencv functions (canny, houghlinesp)
+#define CANNY_T1        100
+#define CANNY_T2        100
+#define CANNY_APERTURE  3
+#define HLINES_THRESH   25
+#define HLINES_MINLINE  100
+#define HLINES_MINGAP   20
+
+// constants for referncing points (x1,y1)(x2,y2) = l1[0,1,2,3]
+#define X1  0
+#define Y1  1
+#define X2  2
+#define Y2  3
+
+// tolerances for determining if two lines are "the same"
+const double HORIZONTAL_TOLERANCE = 0.33;    // how far from slope=0 is considered horizontal
+const double POINT_TOLERANCE = 100;          // how close (in px) for two lines to be "equal" (x-intercept)
+const double SLOPE_TOLERANCE = 0.70;         // how close (in %) two lines slopes must be to be "equal"
+// tolerance for how close to edge to extend to a side of image (in px):
+const int NEAR_EDGE = 100;
 
 // functions to reduce number of lines in image
 // ---
 void remove_horizontal(vector<Vec4i> *);    // removes horizontal lines from the vector<Vec4i>
+void remove_lines(int,int,vector<Vec4i>*);  // removes two lines from a vector of lines
 bool horizontal(Vec4i);                     // determines if a line is horizontal (below tolerance)
 // ---
 vector<Vec4i> combine_lines(vector<Vec4i>); // combines adjacent/seperated lines
+vector<Vec4i> extend_lines(vector<Vec4i>,int,int);  // extends lines to reach end (bottom, edges) of screen
 // ---
-bool greater_than(Vec4i, Vec4i);            // returns true if l1(x,y) > l2(x,y) (hypotenuse)
+bool greater_than(Vec4i, Vec4i);            // returns true if first line is higher up than second
 void swap(Vec4i*, Vec4i*);                  // swaps two lines to pass to adjacent/seperated() correctly
 // ---
+bool same_line(Vec4i, Vec4i);               // returns true if l1,l2 are the "same" line
 bool adjacent(Vec4i, Vec4i);                // returns true if l1,l2 are adjacent
 bool seperated(Vec4i, Vec4i);               // returns true if l1,l2 are seperated but the "same" line
 // ---
 double slope(Vec4i);                        // returns slope of the line passed
 double y_intercept(Vec4i);                  // determine y-intercept of line passed
-double hypotenuse(double,double);           // returns the hypotenuse of two points
+double x_intercept(Vec4i);                  // determine x-intercept of line passed
 int mean(int,int);                          // returns mean between two points
 
 #endif
